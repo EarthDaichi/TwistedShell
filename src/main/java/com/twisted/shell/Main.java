f5bfc4eba8da6518d09c5effb0cb25283f61263e
Earth <pongsakornsinsuwan1@gmail.com>
2026-09-09 06:08:12 +0700

    change Main

--- src/main/java/com/twisted/shell/Main.java
+++ src/main/java/com/twisted/shell/Main.java
@@ -1,16 +1,18 @@
 package com.twisted.shell;
 
 import com.twisted.shell.shell.Shell;
+import com.twisted.shell.core.BuildInfo;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("""
-                ======================================
-                      TwistedShell
-                Git Inspired CLI Framework
-                ======================================
+                    TwistedShell
+                version:""" + BuildInfo.version()
+                );
+        System.out.println("""
+                ____________________________________
                 """);
 
         Shell shell = new Shell();
